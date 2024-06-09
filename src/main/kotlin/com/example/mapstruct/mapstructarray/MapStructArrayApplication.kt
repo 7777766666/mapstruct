ead813085cfe1e092a17e75f0e0806d0767fd89e
777 <goszakupki.aktiv@gmail.com>
2024-06-09 18:06:15 +0300

    start commit

---
+++ src/main/kotlin/com/example/mapstruct/mapstructarray/MapStructArrayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mapstruct.mapstructarray
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapStructArrayApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapStructArrayApplication>(*args)
+}
